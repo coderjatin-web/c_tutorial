9159f2d7d92d6c98b1f70094c8d0497ee1b4be60
Jatin Chourasiya <jatinchourasiya01@gmail.com>
2024-12-16 23:59:47 +0530

    Add files via upload

---
+++ areaofall.c
@@ -0,0 +1,112 @@
+// Area of cirlce
+/*#include<stdio.h>
+int main()
+{
+    float Radius ;
+    printf(" Enter value of Radius : ");
+    scanf(" %f",&Radius);
+    float pie =3.14;  yaha pie = 3.14 se nhi ho rha tha isse ek variable dena hoga varna wo lavaris kehlayga
+    float Area = pie*Radius*Radius;
+    printf("Area of the cirlce is = %f",Area);
+    return 0;
+}
+//area of square
+#include<stdio.h>
+int main()
+{
+    float Side;
+    printf("Enter the value of Side :");
+    scanf("%f",&Side);
+    float Area= Side*Side;
+    printf("The area of %f metre Side square is = %f m^2",Side,Area);
+    return 0;
+
+}*/
+// area of triangle
+/*#include<stdio.h>
+int main()
+{
+    float Base;
+    printf("Enter the value of Base :");
+    scanf("%f",&Base);
+    float Height;
+    printf("Enter the value of Height :");
+    scanf("%f",&Height);
+    float Area= 1/2.0*Base*Height;
+    printf("The area of %f metre base and %f metre height triagle is= %f m^2",Base,Height,Area);
+    return 0;
+
+}*/
+/*#include<stdio.h>
+int main()
+{
+    float Radius ;
+    printf(" Enter value of Radius : ");
+    scanf(" %f",&Radius);
+    float pie =3.14;  
+    float Area = 4*pie*Radius*Radius;
+    printf("Area of the Sphere is = %f",Area);
+    return 0;
+}*/
+
+
+
+
+
+/////////VLOUME KI BARI
+// VOLUME OF SPHERE
+/*#include<stdio.h>
+int main()
+{
+    float Radius ;
+    printf(" Enter value of Radius : ");
+    scanf(" %f",&Radius);
+    float pie =3.14;  
+    float Volume = 4/3.0*pie*Radius*Radius*Radius;
+    printf("Volume of the Sphere is = %f",Volume);
+    return 0;
+}*/
+
+//VOLUME OF CONE
+// #include<stdio.h>
+// int main()
+// {
+//     float Radius ;
+//     printf(" Enter value of Radius : ");
+//     scanf(" %f",&Radius);
+//     float pie =3.14; 
+//     float Height;
+//     printf("Enter the value of Height : ");
+//     scanf(" %f",&Height);
+//     float Volume = 1/3.0*pie*Radius*Radius*Height;
+//     printf("Volume of the Sphere is = %f m^3",Volume);
+//     return 0;
+// }
+
+// VOLUME OF PYRAMID USING ITS AREA PERIMETER AND SLANT HEIGHT
+// #include<stdio.h>
+//  int main(){
+//     float L;
+//     printf("Enter the  Length of base : ");
+//     scanf("%f",&L);
+
+//     float B;
+//     printf("Enter the  breadth of base : ");
+//     scanf("%f",&B);
+
+//     float A = L*B;
+//     printf("\nThe area of base is : %f ",A);
+
+//     float P=2*(L+B);
+//     printf("\nThe Perimetre of base is : %f ",P);
+
+//     float H;
+//     printf("\n\nEnter the Slant height of pyramid : ");
+//     scanf("%f",&H);
+
+//     float V= A+1/2*P*H;
+//     printf("\nThe volume of pyramid with base area :  %f , perimeter of base : %f and slant height :  %f is  :  ",A,P,H);
+//     printf("\n=%f m^2",V);
+//     return 0;
+    
+// }
