ed1086707d31111e09bb335f286a56dad76c7260
Jatin Chourasiya <jatinchourasiya01@gmail.com>
2024-12-19 00:04:41 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,36 @@
+/*#include<stdio.h>
+void main ()
+{
+    int a=10,b,c,d,e;
+    b=--a;
+    c=++b;
+    d=--c;
+    e=c++;
+    printf("%d %d %d %d %d",a ,b,c,d,e);
+    
+}  */
+/*#include<stdio.h>
+void main ()
+{
+    int a=10,b;
+    b= ++a + a++ + ++a + ++a;
+    
+    
+    printf("%d %d ",a ,b);
+    
+}*/ 
+#include<stdio.h>
+void main ()
+{
+    int a=10,b;
+    b= --a + ++a + a-- + a++ + a++ + a-- + --a;
+    
+    
+    printf("%d %d ",a ,b);
+    
+} 
+
+                     
+
+
+
